c006b6dc6d803df0c1d423b20dbfd63c3fcb8e8e
Jan Wedekind (Dr) <jan@wedesoft.de>
2019-09-15 22:36:45 +0100

    static -> inline

--- sfsim2025/list.h
+++ sfsim2025/list.h
@@ -14,20 +14,20 @@ list_t *make_list(void);
 
 void append_gluint(list_t *list, GLuint value);
 
-static GLuint *get_gluint(list_t *list) { return (GLuint *)list->element; }
+inline GLuint *get_gluint(list_t *list) { return (GLuint *)list->element; }
 
 void append_glfloat(list_t *list, GLfloat value);
 
-static GLfloat *get_glfloat(list_t *list) { return (GLfloat *)list->element; }
+inline GLfloat *get_glfloat(list_t *list) { return (GLfloat *)list->element; }
 
 void append_pointer(list_t *list, void *value);
 
-static void **get_pointer(list_t *list) { return (void **)list->element; }
+inline void **get_pointer(list_t *list) { return (void **)list->element; }
 
 void append_point(list_t *list, point_t value);
 
-static point_t *get_point(list_t *list) { return (point_t *)list->element; }
+inline point_t *get_point(list_t *list) { return (point_t *)list->element; }
 
 void append_face(list_t *list, face_t value);
 
-static face_t *get_face(list_t *list) { return (face_t *)list->element; }
+inline face_t *get_face(list_t *list) { return (face_t *)list->element; }
