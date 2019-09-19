5025bb56fb93da9524d21b9a95720d5ff994aebe
Jan Wedekind (Dr) <jan@wedesoft.de>
2019-09-19 22:47:38 +0100

    Negative plane

--- sfsim2025/plane.h
+++ sfsim2025/plane.h
@@ -17,3 +17,9 @@ inline plane_t plane(vector_t point, vector_t normal) {
 inline double plane_distance(plane_t plane, vector_t point) {
   return inner_product(difference(point, plane.point), plane.normal);
 }
+
+inline plane_t negative_plane(plane_t p)
+{
+  vector_t normal = p.normal;
+  return plane(p.point, vector(-normal.x, -normal.y, -normal.z));
+}
--- tests/test_plane.c
+++ tests/test_plane.c
@@ -9,7 +9,15 @@ static MunitResult test_plane_distance(const MunitParameter params[], void *data
   return MUNIT_OK;
 }
 
+// Return plane with inverted normal vector.
+static MunitResult test_negative_plane(const MunitParameter params[], void *data) {
+  plane_t p = plane(vector(2, 3, 5), vector(0, 0, 1));
+  munit_assert_double(negative_plane(p).normal.z, ==, -1);
+  return MUNIT_OK;
+}
+
 MunitTest test_plane[] = {
   {"/plane_distance", test_plane_distance, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
+  {"/negative_plane", test_negative_plane, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
   {NULL             , NULL               , NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}
 };
