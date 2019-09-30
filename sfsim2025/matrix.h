e15e1bf27af233856ede710ea0026555e9c01ab1
Jan Wedekind (Dr) <jan@wedesoft.de>
2019-09-30 08:33:52 +0100

    Matrix vector dot product and transpose

--- sfsim2025/matrix.h
+++ sfsim2025/matrix.h
@@ -1,19 +1,40 @@
 #pragma once
-#include <gc.h>
 #include "vector.h"
 
 
-typedef double matrix_t[3][3];
+typedef struct {
+  double m11;
+  double m12;
+  double m13;
+  double m21;
+  double m22;
+  double m23;
+  double m31;
+  double m32;
+  double m33;
+} matrix_t;
 
-inline matrix_t *matrix(double a11, double a12, double a13, double a21, double a22, double a23, double a31, double a32, double a33) {
-  matrix_t *result = GC_MALLOC_ATOMIC(sizeof(matrix_t));
-  *result[0][0] = a11; *result[0][1] = a12; *result[0][2] = a13;
-  *result[1][0] = a21; *result[1][1] = a22; *result[1][2] = a23;
-  *result[2][0] = a31; *result[2][1] = a32; *result[2][2] = a33;
+inline matrix_t matrix(double m11, double m12, double m13, double m21, double m22, double m23, double m31, double m32, double m33) {
+  matrix_t result;
+  result.m11 = m11; result.m12 = m12; result.m13 = m13;
+  result.m21 = m21; result.m22 = m22; result.m23 = m23;
+  result.m31 = m31; result.m32 = m32; result.m33 = m33;
   return result;
 }
 
 // Cross-product matrix a~ so that a~*b is a x b.
-inline matrix_t *cross_product_matrix(vector_t v) {
+inline matrix_t cross_product_matrix(vector_t v) {
   return matrix(0, -v.z, v.y, v.z, 0, -v.x, -v.y, v.x, 0);
 }
+
+// Dot product of matrix and vector.
+inline vector_t matrix_vector_dot(matrix_t m, vector_t v) {
+  return vector(m.m11 * v.x + m.m12 * v.y + m.m13 * v.z,
+                m.m21 * v.x + m.m22 * v.y + m.m23 * v.z,
+                m.m31 * v.x + m.m32 * v.y + m.m33 * v.z);
+}
+
+// Tramspose a matrix.
+inline matrix_t transpose(matrix_t m) {
+  return matrix(m.m11, m.m21, m.m31, m.m12, m.m22, m.m32, m.m13, m.m23, m.m33);
+};
--- tests/test_matrix.c
+++ tests/test_matrix.c
@@ -1,26 +1,43 @@
 #include "sfsim2025/matrix.h"
-#include "test_helper.h"
 #include "test_matrix.h"
 
 
 static MunitResult test_create(const MunitParameter params[], void *data) {
-  matrix_t *result = matrix(1, 2, 3, 4, 5, 6, 7, 8, 9);
-  munit_assert_double(*result[0][0], ==, 1); munit_assert_double(*result[0][1], ==, 2); munit_assert_double(*result[0][2], ==, 3);
-  munit_assert_double(*result[1][0], ==, 4); munit_assert_double(*result[1][1], ==, 5); munit_assert_double(*result[1][2], ==, 6);
-  munit_assert_double(*result[2][0], ==, 7); munit_assert_double(*result[2][1], ==, 8); munit_assert_double(*result[2][2], ==, 9);
+  matrix_t result = matrix(1, 2, 3, 4, 5, 6, 7, 8, 9);
+  munit_assert_double(result.m11, ==, 1); munit_assert_double(result.m12, ==, 2); munit_assert_double(result.m13, ==, 3);
+  munit_assert_double(result.m21, ==, 4); munit_assert_double(result.m22, ==, 5); munit_assert_double(result.m23, ==, 6);
+  munit_assert_double(result.m31, ==, 7); munit_assert_double(result.m32, ==, 8); munit_assert_double(result.m33, ==, 9);
   return MUNIT_OK;
 }
 
 static MunitResult test_cross_product_matrix(const MunitParameter params[], void *data) {
-  matrix_t *result = cross_product_matrix(vector(1, 2, 3));
-  munit_assert_double(*result[0][0], ==,  0); munit_assert_double(*result[0][1], ==, -3); munit_assert_double(*result[0][2], ==,  2);
-  munit_assert_double(*result[1][0], ==,  3); munit_assert_double(*result[1][1], ==,  0); munit_assert_double(*result[1][2], ==, -1);
-  munit_assert_double(*result[2][0], ==, -2); munit_assert_double(*result[2][1], ==,  1); munit_assert_double(*result[2][2], ==,  0);
+  matrix_t result = cross_product_matrix(vector(1, 2, 3));
+  munit_assert_double(result.m11, ==,  0); munit_assert_double(result.m12, ==, -3); munit_assert_double(result.m13, ==,  2);
+  munit_assert_double(result.m21, ==,  3); munit_assert_double(result.m22, ==,  0); munit_assert_double(result.m23, ==, -1);
+  munit_assert_double(result.m31, ==, -2); munit_assert_double(result.m32, ==,  1); munit_assert_double(result.m33, ==,  0);
+  return MUNIT_OK;
+}
+
+static MunitResult test_matrix_vector_dot(const MunitParameter params[], void *data) {
+  vector_t result = matrix_vector_dot(matrix(1, 2, 3, 4, 5, 6, 7, 8, 9), vector(1, 2, 3));
+  munit_assert_double(result.x, ==, 14);
+  munit_assert_double(result.y, ==, 32);
+  munit_assert_double(result.z, ==, 50);
+  return MUNIT_OK;
+}
+
+static MunitResult test_transpose(const MunitParameter params[], void *data) {
+  matrix_t result = transpose(matrix(1, 2, 3, 4, 5, 6, 7, 8, 9));
+  munit_assert_double(result.m11, ==, 1); munit_assert_double(result.m12, ==, 4); munit_assert_double(result.m13, ==, 7);
+  munit_assert_double(result.m21, ==, 2); munit_assert_double(result.m22, ==, 5); munit_assert_double(result.m23, ==, 8);
+  munit_assert_double(result.m31, ==, 3); munit_assert_double(result.m32, ==, 6); munit_assert_double(result.m33, ==, 9);
   return MUNIT_OK;
 }
 
 MunitTest test_matrix[] = {
-  {"/create"              , test_create              , test_setup_gc, test_teardown_gc, MUNIT_TEST_OPTION_NONE, NULL},
-  {"/cross_product_matrix", test_cross_product_matrix, test_setup_gc, test_teardown_gc, MUNIT_TEST_OPTION_NONE, NULL},
-  {NULL                   , NULL                     , NULL         , NULL            , MUNIT_TEST_OPTION_NONE, NULL}
+  {"/create"              , test_create              , NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
+  {"/cross_product_matrix", test_cross_product_matrix, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
+  {"/matrix_vector_dot"   , test_matrix_vector_dot   , NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
+  {"/transpose"           , test_transpose           , NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
+  {NULL                   , NULL                     , NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}
 };
