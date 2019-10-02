96ee3ac9c10ffc0f05b625ef28576c9776f22ca2
Jan Wedekind (Dr) <jan@wedesoft.de>
2019-10-02 21:40:12 +0100

    Exponential of quaternion

--- sfsim2025/quaternion.h
+++ sfsim2025/quaternion.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <tgmath.h>
 #include "vector.h"
 
 
@@ -14,6 +15,19 @@ inline quaternion_t quaternion(double a, double b, double c, double d) {
   return (quaternion_t){.a = a, .b = b, .c = c, .d = d};
 }
 
+// Create quaternion real component zero and other components taken from the vector.
 inline quaternion_t vector_to_quaternion(vector_t v) {
-  return (quaternion_t){.a = 0, .b = v.x, .c = v.y, .d = v.z};
+  return quaternion(0, v.x, v.y, v.z);
 }
+
+// Create vector from imaginary, jmaginary, and kmaginary component of quaternion.
+inline vector_t quaternion_to_vector(quaternion_t q) {
+  return vector(q.b, q.c, q.d);
+}
+
+// Sinus x divided by x.
+inline double sinc(double x) {
+  return x == 0 ? 1.0 : sin(x) / x;
+}
+
+quaternion_t quaternion_exp(quaternion_t q);
--- tests/test_quaternion.c
+++ tests/test_quaternion.c
@@ -20,8 +20,39 @@ static MunitResult test_from_vector(const MunitParameter params[], void *data) {
   return MUNIT_OK;
 }
 
+static MunitResult test_to_vector(const MunitParameter params[], void *data) {
+  vector_t v = quaternion_to_vector(quaternion(2, 3, 5, 7));
+  munit_assert_double(v.x, ==, 3);
+  munit_assert_double(v.y, ==, 5);
+  munit_assert_double(v.z, ==, 7);
+  return MUNIT_OK;
+}
+
+static MunitResult test_sinc(const MunitParameter params[], void *data) {
+  munit_assert_double_equal(sinc(0), 1, 6);
+  munit_assert_double_equal(sinc(M_PI), 0, 6);
+  return MUNIT_OK;
+}
+
+static MunitResult test_exp_real(const MunitParameter params[], void *data) {
+  munit_assert_double_equal(quaternion_exp(quaternion(1, 0, 0, 0)).a, exp(1), 6);
+  munit_assert_double_equal(quaternion_exp(quaternion(1, M_PI, 0, 0)).a, -exp(1), 6);
+  return MUNIT_OK;
+}
+
+static MunitResult test_exp_imag(const MunitParameter params[], void *data) {
+  munit_assert_double_equal(quaternion_exp(quaternion(1, M_PI / 2, 0, 0)).b, exp(1), 6);
+  munit_assert_double_equal(quaternion_exp(quaternion(1, 0, M_PI / 2, 0)).c, exp(1), 6);
+  munit_assert_double_equal(quaternion_exp(quaternion(1, 0, 0, M_PI / 2)).d, exp(1), 6);
+  return MUNIT_OK;
+}
+
 MunitTest test_quaternion[] = {
   {"/initialize" , test_initialize  , NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
   {"/from_vector", test_from_vector , NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
+  {"/to_vector"  , test_to_vector   , NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
+  {"/sinc"       , test_sinc        , NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
+  {"/exp_real"   , test_exp_real    , NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
+  {"/exp_imag"   , test_exp_imag    , NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
   {NULL          , NULL             , NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}
 };
