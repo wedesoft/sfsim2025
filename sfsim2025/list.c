4c7f6e7e95f9d5cb151f70a71580e3e1c527a3fa
Jan Wedekind <jan@wedesoft.de>
2019-11-05 12:48:54 +0000

    Make more list_t code inline

--- sfsim2025/list.c
+++ sfsim2025/list.c
@@ -3,61 +3,11 @@
 #include <gc.h>
 #include "list.h"
 
-static void grow_list(list_t *list, size_t element_size, bool atomic) {
+void grow_list(list_t *list, size_t element_size, bool atomic) {
   if (list->buffer_size < (list->size + 1) * element_size) {
     list->buffer_size = list->buffer_size ? 2 * list->buffer_size : element_size;
     GLuint *space = atomic ? GC_MALLOC_ATOMIC(list->buffer_size) : GC_MALLOC(list->buffer_size);
     memcpy(space, list->element, list->size * element_size);
     list->element = space;
   };
-}
-
-void append_gluint(list_t *list, GLuint value) {
-  grow_list(list, sizeof(GLuint), true);
-  ((GLuint *)list->element)[list->size++] = value;
-}
-
-void append_glfloat(list_t *list, GLfloat value) {
-  grow_list(list, sizeof(GLfloat), true);
-  ((GLfloat *)list->element)[list->size++] = value;
-}
-
-void append_pointer(list_t *list, void *value) {
-  grow_list(list, sizeof(void *), false);
-  ((void **)list->element)[list->size++] = value;
-}
-
-void append_vector(list_t *list, vector_t value) {
-  grow_list(list, sizeof(vector_t), true);
-  ((vector_t *)list->element)[list->size++] = value;
-}
-
-void append_face(list_t *list, face_t value) {
-  grow_list(list, sizeof(face_t), true);
-  ((face_t *)list->element)[list->size++] = value;
-}
-
-void append_edge(list_t *list, edge_t value) {
-  grow_list(list, sizeof(edge_t), true);
-  ((edge_t *)list->element)[list->size++] = value;
-}
-
-void append_coordinate(list_t *list, coordinate_t value) {
-  grow_list(list, sizeof(coordinate_t), true);
-  ((coordinate_t *)list->element)[list->size++] = value;
-}
-
-void append_contact(list_t *list, contact_t value) {
-  grow_list(list, sizeof(contact_t), true);
-  ((contact_t *)list->element)[list->size++] = value;
-}
-
-void append_body(list_t *list, body_t value) {
-  grow_list(list, sizeof(body_t), true);
-  ((body_t *)list->element)[list->size++] = value;
-}
-
-void append_forces(list_t *list, forces_t value) {
-  grow_list(list, sizeof(forces_t), true);
-  ((forces_t *)list->element)[list->size++] = value;
 }
--- sfsim2025/list.h
+++ sfsim2025/list.h
@@ -19,61 +19,93 @@ static inline list_t make_list(void) {
   return (list_t){.size = 0, .buffer_size = 0, .element = 0};
 }
 
-void append_gluint(list_t *list, GLuint value);
+void grow_list(list_t *list, size_t element_size, bool atomic);
+
+static inline void append_gluint(list_t *list, GLuint value) {
+  grow_list(list, sizeof(GLuint), true);
+  ((GLuint *)list->element)[list->size++] = value;
+}
 
 static inline GLuint *get_gluint(list_t list) {
   return (GLuint *)list.element;
 }
 
-void append_glfloat(list_t *list, GLfloat value);
+static inline void append_glfloat(list_t *list, GLfloat value) {
+  grow_list(list, sizeof(GLfloat), true);
+  ((GLfloat *)list->element)[list->size++] = value;
+}
 
 static inline GLfloat *get_glfloat(list_t list) {
   return (GLfloat *)list.element;
 }
 
-void append_pointer(list_t *list, void *value);
+static inline void append_pointer(list_t *list, void *value) {
+  grow_list(list, sizeof(void *), false);
+  ((void **)list->element)[list->size++] = value;
+}
 
 static inline void **get_pointer(list_t list) {
   return (void **)list.element;
 }
 
-void append_vector(list_t *list, vector_t value);
+static inline void append_vector(list_t *list, vector_t value) {
+  grow_list(list, sizeof(vector_t), true);
+  ((vector_t *)list->element)[list->size++] = value;
+}
 
 static inline vector_t *get_vector(list_t list) {
   return (vector_t *)list.element;
 }
 
-void append_face(list_t *list, face_t value);
+static inline void append_face(list_t *list, face_t value) {
+  grow_list(list, sizeof(face_t), true);
+  ((face_t *)list->element)[list->size++] = value;
+}
 
 static inline face_t *get_face(list_t list) {
   return (face_t *)list.element;
 }
 
-void append_edge(list_t *list, edge_t value);
+static inline void append_edge(list_t *list, edge_t value) {
+  grow_list(list, sizeof(edge_t), true);
+  ((edge_t *)list->element)[list->size++] = value;
+}
 
 static inline edge_t *get_edge(list_t list) {
   return (edge_t *)list.element;
 }
 
-void append_coordinate(list_t *list, coordinate_t value);
+static inline void append_coordinate(list_t *list, coordinate_t value) {
+  grow_list(list, sizeof(coordinate_t), true);
+  ((coordinate_t *)list->element)[list->size++] = value;
+}
 
 static inline coordinate_t *get_coordinate(list_t list) {
   return (coordinate_t *)list.element;
 }
 
-void append_contact(list_t *list, contact_t contact);
+static inline void append_contact(list_t *list, contact_t value) {
+  grow_list(list, sizeof(contact_t), true);
+  ((contact_t *)list->element)[list->size++] = value;
+}
 
 static inline contact_t *get_contact(list_t list) {
   return (contact_t *)list.element;
 }
 
-void append_body(list_t *list, body_t value);
+static inline void append_body(list_t *list, body_t value) {
+  grow_list(list, sizeof(body_t), true);
+  ((body_t *)list->element)[list->size++] = value;
+}
 
 static inline body_t *get_body(list_t list) {
   return (body_t *)list.element;
 }
 
-void append_forces(list_t *list, forces_t value);
+static inline void append_forces(list_t *list, forces_t value) {
+  grow_list(list, sizeof(forces_t), true);
+  ((forces_t *)list->element)[list->size++] = value;
+}
 
 static inline forces_t *get_forces(list_t list) {
   return (forces_t *)list.element;
