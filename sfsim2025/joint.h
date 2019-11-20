f58efa5d730745cd47f1877f569fcb122035fc7a
Jan Wedekind (Dr) <jan@wedesoft.de>
2019-11-20 22:16:07 +0000

    Add new methods to header

--- sfsim2025/joint.h
+++ sfsim2025/joint.h
@@ -42,6 +42,10 @@ large_matrix_t joint_mass(body_t body1, body_t body2, state_t *state1, state_t *
 
 large_vector_t speed_vector(state_t *state1, state_t *state2);
 
+large_vector_t lambda(body_t body1, body_t body2, state_t *state1, state_t *state2, large_matrix_t j, large_vector_t b);
+
+void apply_lambda(large_matrix_t j, large_vector_t lambda, vector_t *impulse1, vector_t *impulse2, vector_t *tau1, vector_t *tau2);
+
 void correcting_impulse(body_t body1, body_t body2, state_t *state1, state_t *state2, large_matrix_t j, large_vector_t b,
                         vector_t *impulse1, vector_t *impulse2, vector_t *tau1, vector_t *tau2);
 
