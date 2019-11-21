90187f66bc067b1eff2e808677dfd96eb1f0a55e
Jan Wedekind (Dr) <jan@wedesoft.de>
2019-11-21 21:35:56 +0000

    Added some documentation

--- sfsim2025/contact.c
+++ sfsim2025/contact.c
@@ -26,18 +26,23 @@ large_matrix_t contact_jacobian(contact_t contact, state_t *state1, state_t *sta
   return result;
 }
 
+// Compute correction for a contact point of two objects.
+// http://image.diku.dk/kenny/download/erleben.05.thesis.pdf
 large_vector_t contact_correction(contact_t contact) {
   large_vector_t result = allocate_large_vector(1);
+  // Only separating corrections are allowed.
   result.data[0] = contact.distance < 0 ? -contact.distance : 0.0;
   return result;
 }
 
+// Compute impulse caused by contact point of two objects.
 void contact_impulse(body_t body1, body_t body2, state_t *state1, state_t *state2, contact_t contact,
                      vector_t *impulse1, vector_t *impulse2, vector_t *tau1, vector_t *tau2) {
   large_matrix_t j = contact_jacobian(contact, state1, state2);
   large_vector_t b = contact_correction(contact);
   large_vector_t lambda_ = lambda(body1, body2, state1, state2, j, b);
   assert(lambda_.rows == 1);
+  // Only separating impulses are allowed.
   if (lambda_.data[0] < 0)
     lambda_.data[0] = 0;
   apply_lambda(j, lambda_, impulse1, impulse2, tau1, tau2);
