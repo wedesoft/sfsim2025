bc565fe2c0c4765db0e842c8792bfc69e5253000
Jan Wedekind (Dr) <jan@wedesoft.de>
2019-11-24 22:19:35 +0000

    Use new world_change method for collision example

--- sfsim2025/collide.c
+++ sfsim2025/collide.c
@@ -30,16 +30,16 @@ static void add_cube_faces(rigid_body_t *body) {
   add_face(body, face(2, 7, 6));
 }
 
-static rigid_body_t *make_cube(double x, double y, double z, double w2, double h2, double d2) {
+static rigid_body_t *make_cube(double w2, double h2, double d2) {
   rigid_body_t *result = make_rigid_body();
-  add_point(result, vector(-w2 + x, -h2 + y, -d2 + z));
-  add_point(result, vector(+w2 + x, -h2 + y, -d2 + z));
-  add_point(result, vector(-w2 + x, -h2 + y, +d2 + z));
-  add_point(result, vector(+w2 + x, -h2 + y, +d2 + z));
-  add_point(result, vector(-w2 + x, +h2 + y, -d2 + z));
-  add_point(result, vector(+w2 + x, +h2 + y, -d2 + z));
-  add_point(result, vector(-w2 + x, +h2 + y, +d2 + z));
-  add_point(result, vector(+w2 + x, +h2 + y, +d2 + z));
+  add_point(result, vector(-w2, -h2, -d2));
+  add_point(result, vector(+w2, -h2, -d2));
+  add_point(result, vector(-w2, -h2, +d2));
+  add_point(result, vector(+w2, -h2, +d2));
+  add_point(result, vector(-w2, +h2, -d2));
+  add_point(result, vector(+w2, +h2, -d2));
+  add_point(result, vector(-w2, +h2, +d2));
+  add_point(result, vector(+w2, +h2, +d2));
   add_cube_faces(result);
   return result;
 }
@@ -61,58 +61,11 @@ void display() {
   glFlush();
 }
 
-void *world_change_prototype(double time, double dt, void *world_, void *data_) {
-  world_t *world = world_;
-  world_t *result = make_world();
-  world_info_t *data = data_;
-  vector_t p[world->states.size]; memset(p, 0, sizeof(p));
-  vector_t t[world->states.size]; memset(t, 0, sizeof(t));
-  state_t *s1 = get_pointer(world->states)[0];
-  state_t *s2 = get_pointer(world->states)[1];
-  rigid_body_t *b1 = make_cube(0, -6370000, 0, 6370000, 6370000, 6370000);
-  rigid_body_t *b2 = transform_body(make_cube(0, 0, 0, w / 2, h / 2, d / 2), s2->orientation, s2->position);
-  body_t body1 = get_body(data->bodies)[0];
-  body_t body2 = get_body(data->bodies)[1];
-  forces_t forces1 = get_forces(data->forces)[0];
-  forces_t forces2 = get_forces(data->forces)[1];
-  list_t contacts_ = contacts(0, 1, b1, b2);
-  vector_t p1[contacts_.size]; memset(p1, 0, sizeof(p1));
-  vector_t p2[contacts_.size]; memset(p2, 0, sizeof(p2));
-  vector_t t1[contacts_.size]; memset(t1, 0, sizeof(t1));
-  vector_t t2[contacts_.size]; memset(t2, 0, sizeof(t2));
-  for (int k=0; k<10; k++) {
-    for (int l=0; l<contacts_.size; l++) {
-      contact_t c = get_contact(contacts_)[l];
-      int i = c.i;
-      int j = c.j;
-      p[i] = vector_subtract(p[i], p1[l]);
-      p[j] = vector_subtract(p[j], p2[l]);
-      t[i] = vector_subtract(t[i], t1[l]);
-      t[j] = vector_subtract(t[j], t2[l]);
-      state_t *prediction1 = predict(s1, body1, forces1, p[i], t[i], dt);
-      state_t *prediction2 = predict(s2, body2, forces2, p[j], t[j], dt);
-      contact_impulse(body1, body2, prediction1, prediction2, c, &p1[l], &p2[l], &t1[l], &t2[l]);
-      p[i] = vector_add(p[i], p1[l]);
-      p[j] = vector_add(p[j], p2[l]);
-      t[i] = vector_add(t[i], t1[l]);
-      t[j] = vector_add(t[j], t2[l]);
-    };
-  };
-  for (int i=0; i<world->states.size; i++) {
-    state_t *s = get_pointer(world->states)[i];
-    body_t b = get_body(data->bodies)[i];
-    forces_t f = get_forces(data->forces)[i];
-    state_t *change = state_change(s, b, f, p[i], t[i], dt);
-    append_pointer(&result->states, change);
-  };
-  return result;
-}
-
 void step() {
   double dt = 0.01;
   int iterations = 3;
   for (int i=0; i<iterations; i++)
-    world = runge_kutta(world, dt / iterations, world_change_prototype, add_worlds, scale_world, &info);
+    world = runge_kutta(world, dt / iterations, world_change, add_worlds, scale_world, &info);
 }
 
 int main(int argc, char *argv[]) {
@@ -135,6 +88,8 @@ int main(int argc, char *argv[]) {
   info.iterations = 4;
   append_body(&info.bodies, body(5.9742e+24, inertia_sphere(5.9742e+24, 6370000)));
   append_body(&info.bodies, body(1.0, inertia_cuboid(1.0, w, h, d)));
+  append_pointer(&info.rigid_bodies, make_cube(6370000, 6370000, 6370000));
+  append_pointer(&info.rigid_bodies, make_cube(w / 2, h / 2, d / 2));
   append_forces(&info.forces, forces(vector(0, 0, 0), vector(0, 0, 0)));
   append_forces(&info.forces, forces(vector(0, -9.81, 0), vector(0, 0, 0)));
   bool quit = false;
