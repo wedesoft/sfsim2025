2d538c4e65d1faf5cc7c134344adad59ca8b8b74
Jan Wedekind <jan@wedesoft.de>
2019-12-06 14:50:18 +0000

    Disable friction

--- sfsim2025/collide.c
+++ sfsim2025/collide.c
@@ -97,7 +97,7 @@ int main(int argc, char *argv[]) {
   append_pointer(&info.rigid_bodies, make_cube(6370000, 6370000, 6370000));
   append_forces(&info.forces, forces(vector(0, 0, 0), vector(0, 0, 0)));
   for (int i=0; i<n; i++) {
-    append_pointer(&world->states, state(vector(0, 2 + 2 * i, 0), vector(0, 0, 0), quaternion(1, 0, 0, 0), vector(0.0, 0.0, 0.0)));
+    append_pointer(&world->states, state(vector(0, 2 + 2 * i, 0), vector(0, 0, 0), quaternion(1, 0, 0, 0), vector(0.0, 0.0, 0.2)));
     append_body(&info.bodies, body(1.0, inertia_cuboid(1.0, w, h, d)));
     append_pointer(&info.rigid_bodies, make_cube(w / 2, h / 2, d / 2));
     append_forces(&info.forces, forces(vector(0, -9.81, 0), vector(0, 0, 0)));
--- sfsim2025/contact.c
+++ sfsim2025/contact.c
@@ -15,7 +15,11 @@ vector_t relative_speed(state_t *state1, state_t *state2, vector_t point) {
 // Construct Jacobian for a contact point of two objects.
 // http://image.diku.dk/kenny/download/erleben.05.thesis.pdf
 large_matrix_t contact_jacobian(contact_t contact, state_t *state1, state_t *state2) {
+#ifdef FRICTION
   large_matrix_t result = allocate_large_matrix(3, 12);
+#else
+  large_matrix_t result = allocate_large_matrix(1, 12);
+#endif
   vector_t u = orthogonal1(contact.normal);
   vector_t v = orthogonal2(contact.normal);
   vector_t r1 = vector_subtract(contact.point, state1->position);
@@ -28,27 +32,39 @@ large_matrix_t contact_jacobian(contact_t contact, state_t *state1, state_t *sta
   vector_t rv2 = cross_product(r2, v);
   double *p0 = result.data;
   *p0++ = -contact.normal.x; *p0++ = -contact.normal.y; *p0++ = -contact.normal.z; p0+= 9;
+#ifdef FRICTION
   *p0++ = u.x; *p0++ = u.y; *p0++ = u.z; p0 += 9;
   *p0++ = v.x; *p0++ = v.y; *p0++ = v.z; p0 += 9;
+#endif
   double *p1 = result.data + 3;
   *p1++ = -rn1.x; *p1++ = -rn1.y; *p1++ = -rn1.z; p1 += 9;
+#ifdef FRICTION
   *p1++ = ru1.x; *p1++ = ru1.y; *p1++ = ru1.z; p1 += 9;
   *p1++ = rv1.x; *p1++ = rv1.y; *p1++ = rv1.z; p1 += 9;
+#endif
   double *p2 = result.data + 6;
   *p2++ = contact.normal.x; *p2++ = contact.normal.y; *p2++ = contact.normal.z; p2+= 9;
+#ifdef FRICTION
   *p2++ = -u.x; *p2++ = -u.y; *p2++ = -u.z; p2 += 9;
   *p2++ = -v.x; *p2++ = -v.y; *p2++ = -v.z; p2 += 9;
+#endif
   double *p3 = result.data + 9;
   *p3++ = rn2.x; *p3++ = rn2.y; *p3++ = rn2.z; p3 += 9;
+#ifdef FRICTION
   *p3++ = -ru2.x; *p3++ = -ru2.y; *p3++ = -ru2.z; p3 += 9;
   *p3++ = -rv2.x; *p3++ = -rv2.y; *p3++ = -rv2.z; p3 += 9;
+#endif
   return result;
 }
 
 // Compute correction for a contact point of two objects.
 // http://image.diku.dk/kenny/download/erleben.05.thesis.pdf
 large_vector_t contact_correction(contact_t contact, bool do_restitution) {
+#ifdef FRICTION
   large_vector_t result = allocate_large_vector(3);
+#else
+  large_vector_t result = allocate_large_vector(1);
+#endif
   double correction;
   if (do_restitution) {
     // Only spearating collision impulses are allowed.
@@ -58,8 +74,10 @@ large_vector_t contact_correction(contact_t contact, bool do_restitution) {
     correction = contact.distance < 0 ? contact.distance : 0.0;
   };
   result.data[0] = correction;
+#ifdef FRICTION
   result.data[1] = 0;
   result.data[2] = 0;
+#endif
   return result;
 }
 
@@ -69,15 +87,19 @@ void contact_impulse(body_t body1, body_t body2, state_t *state1, state_t *state
   large_matrix_t j = contact_jacobian(contact, state1, state2);
   large_vector_t b = contact_correction(contact, do_restitution);
   large_vector_t lambda_ = lambda(body1, body2, state1, state2, j, b);
+#ifdef FRICTION
   assert(lambda_.rows == 3);
+#endif
   // Only separating impulses are allowed.
   if (lambda_.data[0] < 0)
     lambda_.data[0] = 0;
+#ifdef FRICTION
   double friction = sqrt(lambda_.data[1] * lambda_.data[1] + lambda_.data[2] * lambda_.data[2]);
   if (friction > lambda_.data[0] * contact.friction) {
     double factor = lambda_.data[0] * contact.friction / friction;
     lambda_.data[1] *= factor;
     lambda_.data[2] *= factor;
   };
+#endif
   apply_lambda(j, lambda_, impulse1, impulse2, tau1, tau2);
 }
