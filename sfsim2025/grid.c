19407e882445c821a581b7535bfd8b3c918ddf57
Jan Wedekind (Dr) <jan@wedesoft.de>
2020-02-14 22:13:50 +0000

    Initialise shaders for texturized rendering

--- sfsim2025/grid.c
+++ sfsim2025/grid.c
@@ -5,6 +5,49 @@
 #include <SDL.h>
 #include <SDL_opengl.h>
 
+const char *vertexSource = "#version 130\n\
+in mediump vec3 point;\n\
+in mediump vec2 texcoord;\n\
+out mediump vec2 UV;\n\
+void main()\n\
+{\n\
+  gl_Position = vec4(point, 1);\n\
+  UV = texcoord;\n\
+}";
+
+const char *fragmentSource = "#version 130\n\
+in mediump vec2 UV;\n\
+out mediump vec3 fragColor;\n\
+uniform sampler2D tex;\n\
+void main()\n\
+{\n\
+  fragColor = texture(tex, UV).rgb;\n\
+}";
+
+GLuint program;
+
+void printStatus(const char *step, GLuint context, GLuint status) {
+  GLint result = GL_FALSE;
+  glGetShaderiv(context, status, &result);
+  if (result == GL_FALSE) {
+    char buffer[1024];
+    if (status == GL_COMPILE_STATUS)
+      glGetShaderInfoLog(context, 1024, NULL, buffer);
+    else
+      glGetProgramInfoLog(context, 1024, NULL, buffer);
+    if (buffer[0])
+      fprintf(stderr, "%s: %s\n", step, buffer);
+  };
+}
+
+void printCompileStatus(const char *step, GLuint context) {
+  printStatus(step, context, GL_COMPILE_STATUS);
+}
+
+void printLinkStatus(const char *step, GLuint context) {
+  printStatus(step, context, GL_LINK_STATUS);
+}
+
 void display(void) {
   glClear(GL_COLOR_BUFFER_BIT);
   glMatrixMode(GL_MODELVIEW);
@@ -22,6 +65,23 @@ int main(int argc, char *argv[]) {
   SDL_GLContext context = SDL_GL_CreateContext(window);
   glewExperimental = GL_TRUE;
   glewInit();
+
+  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexSource, NULL);
+  glCompileShader(vertexShader);
+  printCompileStatus("Vertex shader", vertexShader);
+
+  GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+  glCompileShader(fragmentShader);
+  printCompileStatus("Fragment shader", fragmentShader);
+
+  program = glCreateProgram();
+  glAttachShader(program, vertexShader);
+  glAttachShader(program, fragmentShader);
+  glLinkProgram(program);
+  printLinkStatus("Shader program", program);
+
   glViewport(0, 0, 640, 480);
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
@@ -35,6 +95,13 @@ int main(int argc, char *argv[]) {
     display();
     SDL_GL_SwapWindow(window);
   };
+
+  glDetachShader(program, vertexShader);
+  glDetachShader(program, fragmentShader);
+  glDeleteProgram(program);
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
   SDL_DestroyWindow(window);
   SDL_Quit();
   return EXIT_SUCCESS;
