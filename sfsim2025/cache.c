bc3843b820dd54b23841109abddb55d199b65354
Jan Wedekind (Dr) <jan@wedesoft.de>
2020-03-04 22:09:59 +0000

    Drop image from cache

--- sfsim2025/cache.c
+++ sfsim2025/cache.c
@@ -7,6 +7,8 @@ image_t cache_image(lru_cache_t *cache, const char *format, int level, int y, in
   char *path = GC_MALLOC_ATOMIC(256);
   snprintf(path, 256, format, level, x, y);
   image_t image = read_image(path);
+  if (cache->list.size >= cache->size)
+    remove_image(&cache->list, 0);
   append_image(&cache->list, image);
   return image;
 }
--- tests/test_cache.c
+++ tests/test_cache.c
@@ -14,12 +14,24 @@ static MunitResult test_add_image(const MunitParameter params[], void *data) {
   lru_cache_t cache = make_image_cache(2);
   image_t image = cache_image(&cache, "cache-%d-%d-%d.png", 2, 3, 5);
   munit_assert_int(cache.list.size, ==, 1);
-  munit_assert_int(image.data[0], ==, 0);
+  munit_assert_int(image.data[0], ==, 2);
+  return MUNIT_OK;
+}
+
+static MunitResult test_drop_image(const MunitParameter params[], void *data) {
+  lru_cache_t cache = make_image_cache(2);
+  cache_image(&cache, "cache-%d-%d-%d.png", 2, 3, 5);
+  cache_image(&cache, "cache-%d-%d-%d.png", 2, 4, 5);
+  cache_image(&cache, "cache-%d-%d-%d.png", 2, 7, 5);
+  munit_assert_int(cache.list.size, ==, 2);
+  munit_assert_int(get_image(cache.list)[0].data[0], ==, 3);
+  munit_assert_int(get_image(cache.list)[1].data[0], ==, 5);
   return MUNIT_OK;
 }
 
 MunitTest test_cache[] = {
-  {"/init"     , test_init     , test_setup_gc, test_teardown_gc, MUNIT_TEST_OPTION_NONE, NULL},
-  {"/add_image", test_add_image, test_setup_gc, test_teardown_gc, MUNIT_TEST_OPTION_NONE, NULL},
-  {NULL        , NULL          , NULL         , NULL            , MUNIT_TEST_OPTION_NONE, NULL}
+  {"/init"      , test_init      , test_setup_gc, test_teardown_gc, MUNIT_TEST_OPTION_NONE, NULL},
+  {"/add_image" , test_add_image , test_setup_gc, test_teardown_gc, MUNIT_TEST_OPTION_NONE, NULL},
+  {"/drop_image", test_drop_image, test_setup_gc, test_teardown_gc, MUNIT_TEST_OPTION_NONE, NULL},
+  {NULL         , NULL           , NULL         , NULL            , MUNIT_TEST_OPTION_NONE, NULL}
 };
