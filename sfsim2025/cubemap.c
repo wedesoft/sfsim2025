3c1893a32616588279fd4b338bedf20628edd521
Jan Wedekind (Dr) <jan@wedesoft.de>
2020-03-17 22:20:52 +0000

    w -> width

--- sfsim2025/cubemap.c
+++ sfsim2025/cubemap.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[]) {
   const int out_level = 0;
   const int n = pow(2, out_level);
   const int tilesize = 256;
-  const int w = 675;
+  const int width = 675;
   cache_t image_cache = make_cache(4);
   cache_t elevation_cache = make_cache(4);
   for (int k=0; k<6;  k++) {
@@ -31,26 +31,26 @@ int main(int argc, char *argv[]) {
             float lat = lattitude(x, y, z);
             int dx[2], dy[2];
             float xfrac[2], yfrac[2];
-            map_pixels_x(lon, w, in_level, &dx[0], &dx[1], &xfrac[0], &xfrac[1]);
-            map_pixels_y(lat, w, in_level, &dy[0], &dy[1], &yfrac[0], &yfrac[1]);
+            map_pixels_x(lon, width, in_level, &dx[0], &dx[1], &xfrac[0], &xfrac[1]);
+            map_pixels_y(lat, width, in_level, &dy[0], &dy[1], &yfrac[0], &yfrac[1]);
             float r[4];
             float g[4];
             float b[4];
             float e[4];
             for (int t=0; t<2; t++) {
               for (int s=0; s<2; s++) {
-                int tx = dx[s] / w;
-                int ty = dy[t] / w;
-                int px = dx[s] % w;
-                int py = dy[t] % w;
+                int tx = dx[s] / width;
+                int ty = dy[t] / width;
+                int px = dx[s] % width;
+                int py = dy[t] % width;
                 image_t image_source = cache_image(&image_cache, "world/%d/%d/%d.png", in_level, ty, tx);
                 elevation_t elevation_source = cache_elevation(&elevation_cache, "elevation/%d/%d/%d.raw", in_level, ty, tx);
-                assert(image_source.width == w);
-                assert(image_source.height == w);
-                assert(elevation_source.width == w);
-                assert(elevation_source.height == w);
-                unsigned char *q1 = image_source.data + (w * py + px) * 3;
-                short int *q2 = elevation_source.data + w * py + px;
+                assert(image_source.width == width);
+                assert(image_source.height == width);
+                assert(elevation_source.width == width);
+                assert(elevation_source.height == width);
+                unsigned char *q1 = image_source.data + (width * py + px) * 3;
+                short int *q2 = elevation_source.data + width * py + px;
                 r[2 * t + s] = q1[0];
                 g[2 * t + s] = q1[1];
                 b[2 * t + s] = q1[2];
